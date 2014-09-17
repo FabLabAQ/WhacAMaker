48d98d15cbad5b7bd0aba6863140456a8c621acb
Tomassino Ferrauto <t_ferrauto@yahoo.it>
2014-09-17 09:49:53 +0200

    Aggiornato il documento (coincide con la versione su google doc del 17/09/2014, ore 9:45)

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -9,32 +9,32 @@ const int tmpLedPWMX = 3;
 const int tmpLedPWMY = 5;
 
 void setup() {
-  // Initialize serial
-  Serial.begin(115200);
+	// Initialize serial
+	Serial.begin(115200);
 
-  // Initialize pins
-  pinMode(joystickP1, INPUT_PULLUP);
-  pinMode(joystickP2, INPUT_PULLUP);
-  
-  pinMode(tmpLed, OUTPUT);
-  pinMode(tmpLedPWMX, OUTPUT);
-  pinMode(tmpLedPWMY, OUTPUT);
+	// Initialize pins
+	pinMode(joystickP1, INPUT_PULLUP);
+	pinMode(joystickP2, INPUT_PULLUP);
+
+	pinMode(tmpLed, OUTPUT);
+	pinMode(tmpLedPWMX, OUTPUT);
+	pinMode(tmpLedPWMY, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:   
-  int pwmValueX = analogRead(joystickX); 
-  int pwmValueY = analogRead(joystickY); 
-  analogWrite(tmpLedPWMX, pwmValueX >> 2);
-  analogWrite(tmpLedPWMY, pwmValueY >> 2);
+	// put your main code here, to run repeatedly:
+	int pwmValueX = analogRead(joystickX);
+	int pwmValueY = analogRead(joystickY);
+	analogWrite(tmpLedPWMX, pwmValueX >> 2);
+	analogWrite(tmpLedPWMY, pwmValueY >> 2);
 
-  if ((digitalRead(joystickP1) == LOW) || (digitalRead(joystickP2) == LOW)) {
-    digitalWrite(tmpLed, HIGH);
-  } else {
-    digitalWrite(tmpLed, LOW);
-  }
-  
-  Serial.print(pwmValueX);
-  Serial.print(" ");
-  Serial.println(pwmValueY);
+	if ((digitalRead(joystickP1) == LOW) || (digitalRead(joystickP2) == LOW)) {
+		digitalWrite(tmpLed, HIGH);
+	} else {
+		digitalWrite(tmpLed, LOW);
+	}
+
+	Serial.print(pwmValueX);
+	Serial.print(" ");
+	Serial.println(pwmValueY);
 }
